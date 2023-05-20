da57e19cbb221cec49b196d391950c5b609fda37
mehmetberkekeceli <berke.keceli96@gmail.com>
2023-05-20 18:57:56 +0300

    UpdateAppFolders

--- BlogLab.UI/src/app/app-routing.module.ts
+++ BlogLab.UI/src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule, UrlHandlingStrategy } from '@angular/router';
+import { BlogEditComponent } from './components/blog-components/blog-edit/blog-edit.component';
+import { BlogComponent } from './components/blog-components/blog/blog.component';
+import { BlogsComponent } from './components/blog-components/blogs/blogs.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PhotoAlbumComponent } from './components/photo-album/photo-album.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'blogs/:id', component: BlogComponent },
+  { path: 'photo-album', component: PhotoAlbumComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard/:id', component: BlogEditComponent, canActivate: [AuthGuard] },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
+
+export class LegacyUrlHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url: import('@angular/router').UrlTree): boolean {
+    return true;
+  }
+
+  extract(url: import('@angular/router').UrlTree): import('@angular/router').UrlTree {
+    return url;
+  }
+
+  merge(newUrlPart: import('@angular/router').UrlTree, rawUrl: import('@angular/router').UrlTree): import('@angular/router').UrlTree {
+    return newUrlPart;
+  }
+
+  handleUrlChange(url: import('@angular/router').UrlTree): void {
+    window.history.pushState(null, '', url.toString());
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: UrlHandlingStrategy, useClass: LegacyUrlHandlingStrategy }]
 })
 export class AppRoutingModule { }
--- BlogLab.UI/src/app/app.component.ts
+++ BlogLab.UI/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BlogLab.UI';
+  title = 'BlogLab-UI';
 }
