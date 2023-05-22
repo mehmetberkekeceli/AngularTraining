39c5a131e5581b8bd4c733d8b80f92742d528bdb
mehmetberkekeceli <berke.keceli96@gmail.com>
2023-05-22 13:47:03 +0300

    UpdateBlogComponents

--- BlogLab.UI/src/app/components/blog-components/blog-card/blog-card.component.ts
+++ BlogLab.UI/src/app/components/blog-components/blog-card/blog-card.component.ts
@@ -1,11 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Blog } from 'app/models/blog/blog.model';
+import { PhotoService } from 'app/services/photo.service';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
   styleUrls: ['./blog-card.component.css']
 })
-export class BlogCardComponent {
-  @Input() blog: Blog = {} as Blog;
+export class BlogCardComponent implements OnInit {
+
+  @Input() blog!: Blog;
+
+  blogPhotoUrl!: string;
+
+  constructor(
+    private router: Router,
+    private photoService: PhotoService
+  ) { }
+
+  ngOnInit(): void {
+    if (!!this.blog.photoId) {
+      this.photoService.get(this.blog.photoId).subscribe(photo => {
+        if (!!photo) {
+          this.blogPhotoUrl = photo.imageUrl;
+        }
+      })
+    }
+  }
+
+  readMore(blogId: number) {
+    this.router.navigate([`/blogs/${blogId}`])
+  }
 }
--- BlogLab.UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
+++ BlogLab.UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Blog } from 'app/models/blog/blog.model';
+import { BlogService } from 'app/services/blog.service';
 
 @Component({
   selector: 'app-famous-blogs',
   templateUrl: './famous-blogs.component.html',
   styleUrls: ['./famous-blogs.component.css']
 })
-export class FamousBlogsComponent {
+export class FamousBlogsComponent implements OnInit {
+
+  famousBlogs: Blog[] = [];
+
+  constructor(
+    private blogService: BlogService
+  ) { }
+
+  ngOnInit(): void {
+    this.blogService.getMostFamous().subscribe(blogs => {
+      this.famousBlogs = blogs;
+    });
+  }
 
 }
